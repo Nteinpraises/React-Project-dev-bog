a0a6ea6a33a39c6fd2bf827ba12a77c644f411fb
Ntein Praises <nteinpraises@gmail.com>
2026-01-31 20:15:38 +0100

    created Homepage and Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const link = 'https://nteinpraises.versel.app/';
+
+
+;
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>APp Components</h1>
+        <Home></Home>
+        <h1>{title}</h1>
+        <a href={link}>my portfolio</a>
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Developer's Tool</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <br />
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
