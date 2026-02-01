32a9ad055999a7d6b0dce6096fc0ead704e24906
Ntein Praises <nteinpraises@gmail.com>
2026-02-01 20:24:54 +0100

    added map function to website

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,19 @@ const Home = () => {
 
    const [name, myName] = useState('Paul');
    const [age, myAge] = useState(33);
+   const [blogs, newBlog] = useState([
+    {title:'my new react website', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, natus', author: 'Moses'},
+    {title:'what we do', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, natus', author: 'Patrick'},
+    {title:'our alumni community', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, natus', author: 'Isaac'},
+    {title:'our future projects', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, natus', author: 'Hamzad'},
+   ])
+   
 
 
     const whenClick=()=>{
         myName('laundry');
         myAge(50);
+        
        
     }
 
@@ -20,6 +28,17 @@ const Home = () => {
             <h2>Homepage</h2>
             <p>{name} is turning {age} years old today</p>
             <button onClick={whenClick}>Click Me</button>
+
+            {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>{blog.body}</p>
+                <p>Written by {blog.author}</p>
+            </div>
+
+            ))}
+
+
         </div>
      );
 }
