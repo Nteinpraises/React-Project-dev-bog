284eb3dad825f6eb2285076ffe40e10a9189782f
Ntein Praises <nteinpraises@gmail.com>
2026-02-01 18:17:08 +0100

    added useState function

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 const Home = () => {
+
+   // let name = 'mario';
+
+   const [name, myName] = useState('Paul');
+   const [age, myAge] = useState(33);
+
+
+    const whenClick=()=>{
+        myName('laundry');
+        myAge(50);
+       
+    }
+
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is turning {age} years old today</p>
+            <button onClick={whenClick}>Click Me</button>
         </div>
      );
 }
