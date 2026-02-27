54fc68a461ce87d559fcc9f3f4ad598d1c9b2e1c
Ntein Praises <nteinpraises@gmail.com>
2026-02-27 06:21:53 +0100

    added notfound(404) page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   const title = 'Welcome to the new blog';
@@ -25,6 +26,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
                <BlogDetails/>
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
           <h1>{title}</h1>
---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return ( 
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <P>That page cannot be found</P>
+            <link to="/">Back to home page...</link>
+        </div>
+     );
+}
+ 
+export default NotFound;
